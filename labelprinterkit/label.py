f69a9fd6e055e6baa89c3d211c8a1296c7a2679e
Gabriel Grant <g@briel.ca>
2020-07-20 16:28:15 -0700

    add centered label class

--- labelprinterkit/label.py
+++ labelprinterkit/label.py
@@ -82,3 +82,36 @@ class Label:
         return img
 
 # print("".join(f"{x:08b}".replace("0", " ") for x in bytes(i)))
+
+class CenteredLabel(Label):
+    def render(self, width=None, height=None) -> Image:
+        """render the Label.
+
+        Args:
+            width: Width request
+            height: Height request
+        """
+        size = self.size
+        img = Image.new("1", size, "white")
+
+        pos = [0, 0]
+
+        for line in self._rendered_items:
+            line_width = sum(item.size[0] for item in line)
+            # to center, offset by half of different between line width and total width
+            pos[0] = (size[0] - line_width) // 2
+            for item in line:
+                box = (*pos, *_coord_add(item.size, pos))
+                img.paste(item, box=box)
+                pos[0] += item.size[0]
+
+            pos[1] += max(i.size[1] for i in line)
+
+        xdim, ydim = img.size
+        print("presize", xdim, ydim, height)
+        xdim = round((height / ydim) * xdim)
+
+        print("calcsize", xdim, ydim)
+        img = img.resize((xdim, height))
+
+        return img
